07041b89493c75fee4d5324acdaed2ed6403414b
AyalaRazo <84156455+AyalaRazo@users.noreply.github.com>
2021-09-11 00:32:10 -0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,26 @@
+<?php
+    include("con_db.php");
+    if(isset($_POST['register'])){
+        if(strlen($_POST['nombre']) >= 1 && strlen($_POST['email']) >= 1){
+            $nombre = trim($_POST['nombre']);
+            $email = trim($_POST['email']);
+            $fechareg = date("d/m/y");
+            $consultar = "INSERT INTO usuarios(nombre, email, fecharegistro) VALUES ('$nombre','$email','$fechareg')";
+            $resultado = mysqli_query($conex,$consultar);
+            if($resultado){
+                ?>
+                <h3 class="ok">Te has registrado correctamente</h3>
+                <?php
+            } else{
+                ?>
+                <h3 class="bad">Ha ocurrido un error</h3>
+                <?php
+            }
+        } else {
+            ?>
+            <h3 class="bad">Por favor complete los campos</h3>
+            <?php
+        }
+    }
+    
+?>
